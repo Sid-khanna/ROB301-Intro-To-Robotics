6e29ab84506f807b523b724da4e3165b0aa438aa
Siddharth Khanna <78490786+Sid-khanna@users.noreply.github.com>
2022-09-27 02:17:02 -0400

    Update lab2.py

--- Lab 2 Pose-to-Pose Control of a Robot/lab2.py
+++ Lab 2 Pose-to-Pose Control of a Robot/lab2.py
@@ -1,1 +1,84 @@
+import rospy
+import math
+import time
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 
+twist = Twist()
+cmd_pub=rospy.Publisher('cmd_vel',Twist,queue_size=1)
+
+
+def stop():
+	twist.linear.x = 0
+       	twist.angular.z = 0
+       	cmd_pub.publish(twist)
+
+
+def move(dist, linear):
+	twist.linear.x = linear
+	twist.angular.z = 0
+	rospy.sleep(1)
+	cmd_pub.publish(twist)
+	rospy.sleep(dist/linear)
+	stop()
+
+
+
+def turn(angle,angular):
+        twist.linear.x = 0
+        twist.angular.z = angular
+        cmd_pub.publish(twist)
+	rospy.sleep(angle/angular)
+	stop()
+
+
+
+def publisher_node():
+    	print('TODO: initialize the publisher node here, \
+            and publish wheel command to the cmd_vel topic')
+
+	#Seteup of robot
+
+	freq = 100
+	rate = rospy.Rate(freq) #10Hz
+	time = 0
+
+	linear = .2
+	angular = .4
+
+
+	#Move forward for 2m
+	dist = 2
+	move(dist, linear)
+
+
+	#Twist 90 degrees
+	angle = math.pi / 2 
+	turn(angle, angular)
+
+
+	#Move forward for .5m
+	dist = .5
+	move(dist, linear)
+
+
+	#Twist 45 degrees
+	angle = math.pi / 4
+	turn(angle, angular)
+
+
+    	pass
+
+
+	
+
+def main():
+    	try:
+        	rospy.init_node('motor')
+        	publisher_node()
+   	except rospy.ROSInterruptException:
+        	pass
+    
+
+if __name__ == '__main__':
+    	main()
