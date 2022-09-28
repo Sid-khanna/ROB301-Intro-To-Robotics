c5abb1a5886a598448e3c90af0c79bb4f0c22e39
Siddharth Khanna <78490786+Sid-khanna@users.noreply.github.com>
2022-09-28 09:41:48 -0400

    Update lab2a.py

--- Lab 2 Pose-to-Pose Control of a Robot/lab2a.py
+++ Lab 2 Pose-to-Pose Control of a Robot/lab2a.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import rospy
 import math
 import time
@@ -10,31 +11,40 @@ cmd_pub=rospy.Publisher('cmd_vel',Twist,queue_size=1)
 
 def stop():
 	twist.linear.x = 0
-       	twist.angular.z = 0
-       	cmd_pub.publish(twist)
+	twist.angular.z = 0
+	cmd_pub.publish(twist)
 
 
 def move(dist, linear):
-	twist.linear.x = linear
-	twist.angular.z = 0
-	rospy.sleep(1)
-	cmd_pub.publish(twist)
-	rospy.sleep(dist/linear)
+	temp = dist/linear
+	t0= rospy.Time.now().to_sec()
+	time = 0
+	while (time<temp):
+		twist.linear.x = linear
+		twist.angular.z = 0
+		cmd_pub.publish(twist)
+		t1 = rospy.Time.now().to_sec()
+		time = t1- t0
 	stop()
 
 
 
 def turn(angle,angular):
-        twist.linear.x = 0
-        twist.angular.z = angular
-        cmd_pub.publish(twist)
-	rospy.sleep(angle/angular)
+	temp = angle/angular
+	t0= rospy.Time.now().to_sec()
+	time = 0
+	while (time<temp):
+		twist.linear.x = 0
+		twist.angular.z = angular
+		cmd_pub.publish(twist)
+		t1 = rospy.Time.now().to_sec()
+		time = t1- t0
 	stop()
 
 
 
 def publisher_node():
-    	print('TODO: initialize the publisher node here, \
+	print('TODO: initialize the publisher node here, \
             and publish wheel command to the cmd_vel topic')
 
 	#Seteup of robot
@@ -45,40 +55,40 @@ def publisher_node():
 
 	linear = .2
 	angular = .4
-
-
+	
+	
 	#Move forward for 2m
 	dist = 2
 	move(dist, linear)
-
+	
 
 	#Twist 90 degrees
 	angle = math.pi / 2 
 	turn(angle, angular)
-
-
+	
+	
 	#Move forward for .5m
 	dist = .5
 	move(dist, linear)
-
-
+	
+	
 	#Twist 45 degrees
 	angle = math.pi / 4
 	turn(angle, angular)
-
-
-    	pass
+	
+	
+	pass
 
 
 	
 
 def main():
-    	try:
-        	rospy.init_node('motor')
-        	publisher_node()
-   	except rospy.ROSInterruptException:
-        	pass
+	try:
+		rospy.init_node('motor')
+		publisher_node()
+	except rospy.ROSInterruptException:
+		pass
     
 
 if __name__ == '__main__':
-    	main()
+	main()
